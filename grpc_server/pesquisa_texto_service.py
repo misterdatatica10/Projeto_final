97ccf51c12226cf3a1744a0d07cbea5d6607adca
Vostro04 <ruieira@ipvc.pt>
2025-01-23 14:20:55 +0000

    Pesquisa de texto, agora utiliza XPATH, lxml adicionado ao requirements.txt

--- grpc_server/pesquisa_texto_service.py
+++ grpc_server/pesquisa_texto_service.py
@@ -1,12 +1,11 @@
 import grpc
 import pesquisa_texto_service_pb2
 import pesquisa_texto_service_pb2_grpc
-import xml.etree.ElementTree as ET
-from settings import MEDIA_PATH  # Importa o caminho do diretório MEDIA_PATH
+from lxml import etree
+from settings import MEDIA_PATH
 
 class PesquisaTextoService(pesquisa_texto_service_pb2_grpc.PesquisaTextoServiceServicer):
     def __init__(self):
-        # Define o caminho completo do arquivo XML no volume configurado
         self.xml_file_path = f"{MEDIA_PATH}/uploaded_file.xml"
 
     def BuscarTexto(self, request, context):
@@ -17,16 +16,16 @@ class PesquisaTextoService(pesquisa_texto_service_pb2_grpc.PesquisaTextoServiceS
     def pesquisar_xml(self, termo):
         resultados = []
         try:
-            # Faz o parsing do XML usando o caminho dinâmico configurado
-            tree = ET.parse(self.xml_file_path)
-            root = tree.getroot()
+            # Use iterparse para processar o XML de forma eficiente
+            context = etree.iterparse(self.xml_file_path, events=("start", "end"))
+            for event, elem in context:
+                if event == "end":  # Apenas processa os elementos completos
+                    if elem.text and termo.lower() in elem.text.lower():
+                        resultados.append(elem.tag)
+                    # Libera memória do elemento após o processamento
+                    elem.clear()
 
-            # Pesquisa no XML por elementos contendo o texto especificado
-            for elem in root.iter():
-                if termo.lower() in elem.text.lower() if elem.text else "":
-                    resultados.append(elem.tag)
-
-        except ET.ParseError as e:
+        except etree.XMLSyntaxError as e:
             print(f"Erro ao processar o XML: {e}")
         except FileNotFoundError:
             print(f"Arquivo XML não encontrado: {self.xml_file_path}")
